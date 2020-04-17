1c8b31f27b35e11f5c3223c7bf7506b53c80e5b8
reybel24 <reybel24@gmail.com>
2020-04-17 12:38:01 -0400

    fix button border blurry issue

--- src/components/UI/RoundedButton.vue
+++ src/components/UI/RoundedButton.vue
@@ -30,16 +30,18 @@ export default {
   padding: 8px 11px 8px 11px;
   display: flex;
   background-color: white;
-  box-shadow: 0 0 3px $blue;
   border-radius: 15px;
   color: $blue;
   transition: 0.12s;
   font-size: 0.75em;
   align-items: center;
+  border: 1px solid $blue;
+  opacity: .8;
 }
 .btn:hover {
   background-color: $blue;
   color: white;
+  opacity: 1;
 }
 .icon {
     font-size: .9em;
